d997831ee61ff4778d1ff5eb5e5448a1caa43aee
Maxwell Paulo <Maxwell@Maxwells-MacBook-Pro.local>
2022-09-06 18:24:14 -0300

    coutries list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,13 @@
+import { Navbar } from './components/Navbar';
+import { CountriesList } from './components/CountriesList';
+
 function App() {
-  return <div className="App">teste</div>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <CountriesList></CountriesList>
+    </>
+  );
 }
 
 export default App;
